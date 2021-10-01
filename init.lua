9b6beff9ad80931c21fedca64f335a8077821ab3
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2021-10-01 11:36:35 +0200

    Remove stale require of scrollbar config.

--- init.lua
+++ init.lua
@@ -50,7 +50,6 @@ require 'my.bufdel'
 require 'my.fugitive'
 require 'my.grepper'
 require 'my.matchup'
-require 'my.scrollbar'
 require 'my.closetag'
 require'my.indent'.setup {width = 2}
 require'my.terminal'.setup {perc = 0.8, keymap = '<leader>t'}
