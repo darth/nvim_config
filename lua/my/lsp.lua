7fc0d802f03676e02d9f41cec29fd794046e4104
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2021-12-12 23:03:24 +0100

    Add jsonls to the list of lsp servers.

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -59,7 +59,7 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {'pyright', 'sourcegraph_js', 'yamlls'}
+local servers = {'pyright', 'sourcegraph_js', 'yamlls', 'jsonls'}
 for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = on_attach} end
 
 nvim_lsp['ccls'].setup {
