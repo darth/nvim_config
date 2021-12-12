2d71fdd2a435e37d4baab2d3edfc0679a581444a
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2021-12-12 23:06:24 +0100

    Override capabilities for all lsp servers.

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -59,8 +59,8 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {'pyright', 'sourcegraph_js', 'yamlls', 'jsonls'}
-for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = on_attach} end
+local servers = {'pyright', 'sourcegraph_js', 'rust-analyzer', 'yamlls', 'jsonls'}
+for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = on_attach, capabilities = capabilities} end
 
 nvim_lsp['ccls'].setup {
   capabilities = capabilities,
