077eeeecc9e4ab1ac8373ebd404ef10d9d7898d2
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2021-12-12 23:55:14 +0100

    Fix rust-analyzer lsp config name.

--- lua/my/lsp.lua
+++ lua/my/lsp.lua
@@ -59,7 +59,7 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = {'pyright', 'sourcegraph_js', 'rust-analyzer', 'yamlls', 'jsonls'}
+local servers = {'pyright', 'sourcegraph_js', 'rust_analyzer', 'yamlls', 'jsonls'}
 for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = on_attach, capabilities = capabilities} end
 
 nvim_lsp['ccls'].setup {
